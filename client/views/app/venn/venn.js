d50a0ac72c32ffa712d4398fe2736e1cd6e1a876
perjo927 <perjo927@student.liu.se>
2015-05-27 19:44:06 +0200

    New chartist chart

--- client/views/app/venn/venn.js
+++ client/views/app/venn/venn.js
@@ -1,7 +1,32 @@
+var data = {
+    labels: ['Bananas', 'Apples', 'Grapes'],
+    series: [20, 15, 40]
+};
+
+var options = {
+    labelInterpolationFnc: function(value) {
+        return value[0]
+    }
+};
+
+var responsiveOptions = [
+    ['screen and (min-width: 640px)', {
+        chartPadding: 30,
+        labelOffset: 100,
+        labelDirection: 'explode',
+        labelInterpolationFnc: function(value) {
+            return value;
+        }
+    }],
+    ['screen and (min-width: 1024px)', {
+        labelOffset: 80,
+        chartPadding: 20
+    }]
+];
 
 /* */
 Template.venn.onRendered(function () {
-
+    new Chartist.Pie('.ct-chart', data, options, responsiveOptions);
 });
 
 /* */

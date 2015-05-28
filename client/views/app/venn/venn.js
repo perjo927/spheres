8f6c55d2479346b8d0891b321d2565e388e4da69
perjo927 <perjo927@student.liu.se>
2015-05-28 19:23:00 +0200

    Improved Venn diagram

--- client/views/app/venn/venn.js
+++ client/views/app/venn/venn.js
@@ -1,32 +1,7 @@
-var data = {
-    labels: ['Bananas', 'Apples', 'Grapes'],
-    series: [20, 15, 40]
-};
 
-var options = {
-    labelInterpolationFnc: function(value) {
-        return value[0]
-    }
-};
-
-var responsiveOptions = [
-    ['screen and (min-width: 640px)', {
-        chartPadding: 5,
-        labelOffset: 40,
-        labelDirection: 'explode',
-        labelInterpolationFnc: function(value) {
-            return value;
-        }
-    }],
-    ['screen and (min-width: 1024px)', {
-        labelOffset: 80,
-        chartPadding: 20
-    }]
-];
 
 /* */
 Template.venn.onRendered(function () {
-    new Chartist.Pie('.ct-chart', data, options, responsiveOptions);
 });
 
 /* */

d307c33dbb33dae8cafa26c85247f2dc4752a203
perjo927 <perjo927@student.liu.se>
2015-07-12 12:02:20 +0200

    Vamped up the about section

--- server/view_models/venn.model.js
+++ server/view_models/venn.model.js
@@ -1,5 +1,29 @@
+var textSection = {
+    title: "Per",
+    subTitle: "The cross-functional web avant-gardist",
+    text: "During the first weeks working for Betsson I was told to choose a value word and I thought " +
+        "pretty hard about it. Since I have a tendency to lean towards the borders of change " +
+        "'avant-garde' was picked, which signifies my mindset quite well. It is easy to get " +
+        "caught up in buzzwords though and I'm working hard on catching up with the past too. " +
+        "My ambition is to be a truly cross-functional web developer in order to flow with " +
+        "the ever changing situations you face as a web developer." +
+        "With a Bachelor degree in programming from Linkoping university I went on to finish my " +
+        "thesis work at Ericsson. Since August 2014 I've been lucky enough to work for Betsson,  " +
+        "which has turned out to be the perfect place to grow as a web developer in all regards. "
+};
+
+var vennData = {
+    W: "Programming from joy",
+    N: "UX geek",
+    E: "Creativity + Innovation",
+    M: "Per",
+    S: "Desire for knowledge",
+    SW: "Bedroom enthusiast",
+    SE: "Computer scientist"
+};
 
 /* */
 VM.sections.venn.docs = {
-    foo: "bar"
+    textSection: textSection,
+    vennData: vennData
 };
